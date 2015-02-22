c0dae5ce80b8b8b1c7d09b801772b23f8cd8b91b
Emelx <m_landi@yahoo.com>
2015-02-22 11:24:31 +0100

    Programming Assignment 2 for R Programming - made minor comments adjustments

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,7 @@
 ## This function creates an environment so that the inverse of a given matrix
 ## can be cached. In this environment get/set functions are defined that use
 ## variables in the parent environment.
-## The function returns a special matrix that is a list of functions which
+## The function returns a "special matrix" that is a list of functions which
 ## allow to set/get the original matrix and to set/get the inverse
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -24,7 +24,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Massimo Landi 2015-02-22
-## This function calculates the inverse of the special matrix returned by
+## This function calculates the inverse of the "special matrix" returned by
 ## the above function.
 ## The inverse is computed only if a cached value is missing and then the
 ## cached value is used in subsequent calls. 
